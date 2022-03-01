87a8ae847af69c04cf2119071402cce1e322fbbb
unknown <crsn1111@gmail.com>
2022-03-01 22:03:38 +0900

    redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
 
 function App() {
-  return <div></div>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home(props) {
+  console.log(props);
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
@@ -10,15 +12,27 @@ function Home() {
     setText("");
   }
   return (
-    <>
+    <React.Fragment>
       <h1>To Do</h1>
       <form onSubmit={onSubmit}>
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
       <ul></ul>
-    </>
+    </React.Fragment>
   );
 }
 
-export default Home;
+function mapStateToProps(state) {
+  return {
+    toDos: state,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
