c80db5cf9901f68796f167daada98a084451b9c4
unknown <crsn1111@gmail.com>
2022-03-03 22:15:02 +0900

    redux

--- src/store.js
+++ src/store.js
@@ -3,6 +3,20 @@ import { createStore } from "redux";
 const ADD = "ADD";
 const DELETE = "DELETE";
 
+export const addTodo = (text) => {
+  return {
+    type: ADD,
+    text,
+  };
+};
+
+export const deleteTodo = (text) => {
+  return {
+    type: DELETE,
+    text,
+  };
+};
+
 const reducer = (state = ["hi"], action) => {
   switch (action.type) {
     case ADD:
